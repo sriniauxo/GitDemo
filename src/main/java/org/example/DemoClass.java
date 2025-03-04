6fe99f1b0a8f264fa4c25fab34d0f48ad3136763
Srinivas <srinivas.aalwala@gmail.com>
2025-03-04 22:02:05 +0530

    Added by Srinivas in India Time Zone

---
+++ src/main/java/org/example/DemoClass.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("second program  y srinivas");
+	}
+
+}
